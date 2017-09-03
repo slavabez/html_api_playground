0f7d08a1449f31dcb4e84cb3d690b629b1578897
Slava Bezgachev <bezgachev@gmail.com>
2017-09-03 23:20:35 +0100

    Added the iffe for Offline API experiments

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import "./css/main.css";
 import geolocation from './geolocation';
+import offline from './offline';
 
 
 
 document.addEventListener("DOMContentLoaded", function(event) {
     geolocation();
-
+    offline();
 });
 
 
---
+++ src/offline.js
@@ -0,0 +1,5 @@
+
+
+export default function initOffline(){
+    console.log('Offline started');
+}
